88c19f55768642763d73272b830a882726aae6cd
erDeep640390 <erdeep64930@gmail.com>
2023-09-07 14:46:44 +0530

    svg from razorpay has been added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import { AboutNavigation } from "../components/AboutSection/AboutNavigation"
+import { SomeSVGstyle } from "../components/AboutSection/SomeSVGstyle"
 
 
 const About = () => {
   return (
     <div className="mt-50 py-12">
 <AboutNavigation />
+<SomeSVGstyle />
 
     </div>
   )
